269d153c2a016d26434c74fbe4d7071ba4c54e30
Chach Sikes <chachasikes@gmail.com>
2012-03-26 21:48:33 -0700

    Added wax library. Map eases, and loads data.

--- js/map.spotlight.js
+++ js/map.spotlight.js
@@ -12,12 +12,20 @@ var data = {};
 var panelMarkup = {};
 
 window.onload = function() {
+  
+  $('div#panel-container div#panel').hide();
+  
   newPlayMap.initMap();
+  newPlayMap.mapCustomizations();  
+  newPlayMap.mapButtons();
+};
+
+newPlayMap.mapCustomizations = function () {
+  map.setZoomRange(0, 7);
 };
 
 newPlayMap.initMap = function() {
-    var provider = new MM.TemplatedLayer("http://ecn.t{S}.tiles.virtualearth.net/tiles/r{Q}?g=689&mkt=en-us&lbl=l0&stl=m", [0,1,2,3,4,5,6,7]);
-
+    var provider = new MM.TemplatedLayer("http://ecn.t{S}.tiles.virtualearth.net/tiles/r{Q}?g=689&mkt=en-us&lbl=l0&stl=m", [0,1,2,3,4,5,6,7]);    
     map = new MM.Map("map", provider, null, [
                 new easey.DragHandler(),
                 new easey.TouchHandler(),
@@ -25,10 +33,29 @@ newPlayMap.initMap = function() {
                 new easey.MouseWheelHandler()
             ]);
     map.setCenterZoom(new MM.Location(36.9818, -121.9575), 10);
-
+    // 3000 views per month free mapbox account
+ 
 /*
+var url = 'http://a.tiles.mapbox.com/v3/newplaymap.map-m3r2xeuk.jsonp';
+
+wax.tilejson(url, function(tilejson) {
+  var m = new MM.Map('map',
+    new wax.mm.connector(tilejson),
+    new MM.Point(700,400));
+
+  m.setCenterZoom(new MM.Location(tilejson.center[1],
+    tilejson.center[0]),
+    tilejson.center[2] - 3);
+
+  wax.mm.zoomer(m).appendTo(m.parent);
+  wax.mm.interaction(m);
+});
+*/
+ 
+    
     // Syntax example. Seeing if Wax works.
-    wax.tilejson('http://a.tiles.mapbox.com/v3/mapbox.world-bright.jsonp',
+/*
+    wax.tilejson('http://a.tiles.mapbox.com/v3/newplaymap.map-m3r2xeuk.jsonp',
         function(tj) {
         map = new com.modestmaps.Map('map',
             new wax.mm.connector(tj), null, [
@@ -70,7 +97,7 @@ newPlayMap.onLoadEventMarkers = function(collection) {
     for (var i = 0; i < len; i++) {
         var feature = features[i],
             type = feature.properties["related_play_id"],
-            marker = document.createElement("a");
+            marker = document.createElement("div");
 
         marker.feature = feature;
         marker.type = type;
@@ -105,6 +132,7 @@ newPlayMap.onLoadEventMarkers = function(collection) {
         // listen for mouseover & mouseout events
         MM.addEvent(marker, "mouseover", newPlayMap.onMarkerOver);
         MM.addEvent(marker, "mouseout", newPlayMap.onMarkerOut);
+        MM.addEvent(marker, "click", newPlayMap.onMarkerClick);
     }
 
     // tell the map to fit all of the locations in the available space
@@ -120,46 +148,70 @@ newPlayMap.getMarker = function(target) {
 };
 
 newPlayMap.onMarkerOver = function(e) {
-    var marker = newPlayMap.getMarker(e.target);
+  var marker = newPlayMap.getMarker(e.target);
 
-    if (marker) {
+  if (marker) {
+      var type = marker.type;
 
-        newPlayMap.updatePanel(marker);
+      if (type in locationsByType) {
+          spotlight.addLocations(locationsByType[type] || []);
+          spotlight.parent.className = "active";
 
-        var type = marker.type;
-        // console.log("over:", type);
-        if (type in locationsByType) {
-            spotlight.addLocations(locationsByType[type] || []);
-            spotlight.parent.className = "active";
-        } else {
-            spotlight.parent.className = "inactive";
-        }
-    }
+          $('div#panel-container div#panel').show();
+                      
+          // Update the panel data.
+          newPlayMap.updatePanel(marker, locationsByType[type]);
+
+          
+      } else {
+          spotlight.parent.className = "inactive";
+      }
+  }
 };
 
 newPlayMap.onMarkerOut = function(e) {
-    var marker = newPlayMap.getMarker(e.target);
-    if (marker) {
-        var type = marker.type;
-        // console.log("out:", type);
-        spotlight.removeAllLocations();
-        spotlight.parent.className = "inactive";
-    }
+
+  var marker = newPlayMap.getMarker(e.target);
+  if (marker) {
+      var type = marker.type;
+
+      spotlight.removeAllLocations();
+      
+      $('div#panel-container div#panel').hide();
+      
+      spotlight.parent.className = "inactive";
+  }
+
+  return false;
 };
 
-newPlayMap.updatePanel = function(marker) {
+newPlayMap.onMarkerClick = function(e) {
+  // Address History
+  // Set URL
+  
+  console.log("click");
+    
 
-  console.log(marker);
+  return false;
+};
+
+// @TODO This doesn't seem to connect to any modest map click functions.
+// Maybe we do not need something like this. will think about it.
+newPlayMap.onMarkerClickOut = function(e) {
+
+  console.log("click out");
+
+};
+
+newPlayMap.updatePanel = function(marker, relatedData) {
+console.log(relatedData);
+  // @TOOD Right now this will just load events.
   var event_id = marker.getAttribute("event_id");
   feature = newPlayMap.loadDataObject(data.eventData, event_id);
   var panelData = document;
-
-  console.log(feature);
   
-/*   $('#panel-container').html(event_id); */
-  
-  
-  newPlayMap.eventTemplate(feature, "event")
+  // Load event data into the event template.
+  newPlayMap.panelTemplate(feature, "event")
 };
 
 newPlayMap.loadDataObject = function(collection, id) {
@@ -180,8 +232,7 @@ newPlayMap.loadDataObject = function(collection, id) {
 
 };
 
-newPlayMap.eventTemplate = function(data, type) {
-console.log(data);
+newPlayMap.panelTemplate = function(data, type) {
   var type = type;
   var container = $('#panel-container .' + type);
   if (panelMarkup[type] === null || panelMarkup[type] === undefined) {
@@ -189,35 +240,21 @@ console.log(data);
   }
 
   var content = {};
-  
   container.empty();
-
-
   // http://api.jquery.com/jquery.tmpl/
-  
-  $.template( "eventTemplate", panelMarkup[type]);        
 
-  $.tmpl("eventTemplate", data["properties"])
+  // @TODO Data may need some escaping.
+  $.template( type + "Template", panelMarkup[type]);        
+  $.tmpl(type + "Template", data["properties"])
     .appendTo(container); 
-    
-
-
-/*   for (var i in data) { */
-/*     for (key in almanac.content[i]) { */
-/*       if(data.content[i][key].type == type) { */
-/*        foodContent.key = key; */
-      
-/*         for (item in almanac.content[i][key]) { */
-/*           if(item == "title") { */
-/*             foodContent.title = almanac.content[i][key][item]; */
-/*           } */
-/*         } */
-    /*
-      $.tmpl("eventTemplate", data["properties"])
-          .appendTo(container);
-*/
-/*       } */
-/*     } */
-/*   } */
-
 }
+
+newPlayMap.mapButtons = function() {
+  $(".zoom-in").click(function() {
+    map.zoomBy(1);
+  });
+
+  $(".zoom-out").click(function() {
+    map.zoomBy(-2);
+  });
+};
