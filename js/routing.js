0848b585f1957544a839aea3511fb0a8c46eac25
Chach Sikes <chachasikes@gmail.com>
2012-04-09 14:31:41 -0700

    loading an attribute from a data object

--- js/routing.js
+++ js/routing.js
@@ -1,13 +1,44 @@
 var routing = {};
 
+// @TODO routing will be loaded everytime a dataset is loaded, it should be called.
+
 routing.load = function() {
-  console.log(data);
-};
+
+
+filters.organizations();
 
 /*
-$('.typeahead').typeahead({
-  source: data.eventData.title
-}
-
-)
+$('.typeahead').typeahead(
+  {
+  source: data.organizationData.title
+  }
+);
 */
+};
+
+
+
+var filters={};
+filters.data = {};
+filters.data.organizations = {};
+
+filters.organizations = function() {
+console.log(data.organizationData);
+    var features = data.organizationData.features,
+        len = features.length,
+        locations = [];
+
+filters.data.organizations.titles = {};
+
+    for (var i = 0; i < len; i++) {
+            var feature = features[i],
+            id = feature.properties["organization_id"];
+
+/* filters.data.organizations.titles += {feature.title: id};     */
+console.log(feature.properties.name);
+
+
+    }
+/* console.log(filters.data.plays.titles); */
+
+};
