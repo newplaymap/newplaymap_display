e102e80801573a1118c8a859674fbc4e33ec77ec
Colin Sagan <colin@quilted.coop>
2012-04-17 14:53:47 -0700

    Create Organizations filter api file. Returns all orgs with name and id.

---
+++ api/organizations_index.php
@@ -0,0 +1,51 @@
+<?php
+include('../../../authentication/newplaymap_authentication.php');
+connectMongo(false);
+
+if(!empty($_GET['page'])){
+ $page = $_GET['page'];
+}
+else{
+  $page = 0;
+}
+$page_items = 150;
+
+$collection = $m->newplaymap->organizations;
+
+// find everything in the collection
+$cursor = $collection->find()->sort(array("properties.name" => 1));
+$count = $cursor->count();
+
+header('Access-Control-Allow-Origin: *.newplaymap.org | localhost | *.chachaville.com');
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
+header("Last-Modified: " . gmdate( "D, d M Y H:i:s" ) . "GMT"); 
+header("Cache-Control: no-cache, must-revalidate"); 
+header("Pragma: no-cache");
+header("Content-type: application/json");
+ $page;
+$json = '{"organizations":[ ' ;
+
+$i = 0;
+
+// iterate through the results
+foreach ($cursor as $obj) {
+  if(!empty($obj['id'])) {
+    if($i > 0) {
+     $json .= ',';
+    }
+
+    $json .= json_encode(array(
+      'id' => $obj['id'],
+      'name' => $obj['properties']['name'],
+    ));
+  
+    $i++;
+  }
+}
+/* $json .= "," . json_encode(array('count' => $collection->count())); */
+
+$json .= '], "count" : ' . $count . '}';
+
+echo $json;
+
+?>
