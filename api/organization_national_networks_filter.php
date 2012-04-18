8adeb498abb10bcc6520a3b0279beed6c8294e1d
Colin Sagan <colin@quilted.coop>
2012-04-18 14:56:12 -0700

    Add API file for National Network Affiliatiosn.

---
+++ api/organization_national_networks_filter.php
@@ -0,0 +1,46 @@
+<?php
+include('../../../authentication/newplaymap_authentication.php');
+connectMongo(false);
+
+$collection = $m->newplaymap->organizations;
+
+$national_networks = (!empty($_GET['national_networks'])) ? $_GET['national_networks'] : null;
+
+if ($national_networks == null) {
+  return;
+}
+
+// find everything in the collection
+$cursor = $collection->find(array("properties.national_networks.network" => $national_networks))->sort(array("properties.name" => 1));
+$count = $cursor->count();
+
+header('Access-Control-Allow-Origin: *.newplaymap.org | localhost | *.chachaville.com');
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
+header("Last-Modified: " . gmdate( "D, d M Y H:i:s" ) . "GMT"); 
+header("Cache-Control: no-cache, must-revalidate"); 
+header("Pragma: no-cache");
+header("Content-type: application/json");
+ $page;
+$json = '{"name": "national_networks_filter","type":"FeatureCollection","features":[ ' ;
+
+$i = 0;
+
+// iterate through the results
+foreach ($cursor as $obj) {
+  if(!empty($obj['id'])) {
+    if($i > 0) {
+     $json .= ',';
+    }
+
+    $json .= json_encode($obj);
+  
+    $i++;
+  }
+}
+/* $json .= "," . json_encode(array('count' => $collection->count())); */
+
+$json .= '], "count" : ' . $count . '}';
+
+echo $json;
+
+?>
