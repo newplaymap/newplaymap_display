70a736915a7f5acf68dc123e3259fdfe7496253b
Colin Sagan <colin@quilted.coop>
2012-04-20 02:34:33 -0700

    Now that there is a plays collection in mongo, use that for the index instead for parsing events.

--- api/plays_index.php
+++ api/plays_index.php
@@ -2,9 +2,7 @@
 include('../../../authentication/newplaymap_authentication.php');
 connectMongo(false);
 
-// @TODO: This might be better off as a new collection instead of looking through all the events. That way it could also be alphabetized on the way in from Drupal instead of mongo which places lower-case letters after all the upper-case letters.
-
-$collection = $m->newplaymap->events;
+$collection = $m->newplaymap->plays;
 
 // find everything in the collection
 $cursor = $collection->find()->sort(array("properties.play_title" => 1));
@@ -22,18 +20,14 @@ $json = '[ ' ;
 $i = 0;
 
 // iterate through the results
-$unique_plays = array();
 foreach ($cursor as $obj) {
   if(!empty($obj['id']) && !empty($obj['properties']['play_title'])) {
-    if (!in_array($obj['properties']['play_title'], $unique_plays)) {
-      if($i > 0) {
-       $json .= ',';
-      }
-      $unique_plays[] = $obj['properties']['play_title'];
-      $json .= json_encode($obj['properties']['play_title']);
-      $i++;
+    if($i > 0) {
+     $json .= ',';
     }
 
+    $json .= json_encode($obj['properties']['play_title']);
+    $i++;
   }
 }
 /* $json .= "," . json_encode(array('count' => $collection->count())); */
