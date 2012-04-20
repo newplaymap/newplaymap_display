c72079d92ecdf08e3ccf04409b7c0cfac91ca6fd
Colin Sagan <colin@quilted.coop>
2012-04-20 02:35:37 -0700

    Create an plays filter api file for returning plays based on it's name.

---
+++ api/plays_filter.php
@@ -0,0 +1,46 @@
+<?php
+include('../../../authentication/newplaymap_authentication.php');
+connectMongo(false);
+
+$play_title = (!empty($_GET['play_title'])) ? $_GET['play_title'] : null;
+
+if ($play_title == null) {
+  return;
+}
+
+$collection = $m->newplaymap->plays;
+
+// find everything in the collection
+$cursor = $collection->find(array("properties.play_title" => $play_title))->sort(array("properties.play_title" => 1));
+$count = $cursor->count();
+
+header('Access-Control-Allow-Origin: *.newplaymap.org | localhost | *.chachaville.com');
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
+header("Last-Modified: " . gmdate( "D, d M Y H:i:s" ) . "GMT"); 
+header("Cache-Control: no-cache, must-revalidate"); 
+header("Pragma: no-cache");
+header("Content-type: application/json");
+ $page;
+$json = '{"name": "plays_filter","type":"FeatureCollection","features":[  ' ;
+
+$i = 0;
+
+// iterate through the results
+$unique_plays = array();
+foreach ($cursor as $obj) {
+  if(!empty($obj['id'])) {
+    if($i > 0) {
+     $json .= ',';
+    }
+
+    $json .= json_encode($obj);
+    $i++;
+  }
+}
+/* $json .= "," . json_encode(array('count' => $collection->count())); */
+
+$json .= '], "count" : ' . $count . '}';
+
+echo $json;
+
+?>
