622a80818e92a7cea4e7cbe77286c2d7f49ca204
Colin Sagan <colin@quilted.coop>
2012-05-04 01:39:26 -0700

    Revert "Make city state api return a single big json object with all three lists of city/states."
    This reverts commit 5afdad02d94ae4e4662a663548e65666f180313a.

--- api/city_state_index.php
+++ api/city_state_index.php
@@ -24,20 +24,10 @@ switch ($type) {
 
 if($type !== null) {
   // Find all
-  $cursor = get_city_states($collection);
+  $cursor = $collection->find(array('properties.city' => array('$ne' => ''), 'properties.state' => array('$ne' => '')))->sort(array("properties.state" => 1));
 }
 else {
-  // Return an object with all three
-  $all_collections  = array('artists', 'organizations', 'events');
-  $all_cursors = array();
-  foreach($all_collections as $collection_name) {
-    $all_cursors[$collection_name] = get_city_states($m->newplaymap->$collection_name);
-  }
-}
-
-function get_city_states($collection) {
-  $cursor = $collection->find(array('properties.city' => array('$ne' => ''), 'properties.state' => array('$ne' => '')))->sort(array("properties.state" => 1));
-  return $cursor;
+  return;
 }
 
 
@@ -52,36 +42,32 @@ header("Cache-Control: no-cache, must-revalidate");
 header("Pragma: no-cache");
 header("Content-type: application/json");
  $page;
-$json = '{ ' ;
+$json = '[ ' ;
 
-$cursor_index = 0;
+$i = 0;
+
 // iterate through the results
-foreach ($all_cursors as $cursor) {
-  $all_keys = array_keys($all_cursors);
-  $json .= $all_keys[$cursor_index] . ': [';
-  $city_state_list = array();
-  $i = 0;
-  foreach ($cursor as $obj) {
-    if (!in_array($obj['properties']['city_state'], $city_state_list) && $obj['properties']['city_state'] != null) {
-      if($i > 0) {
-       $json .= ',';
-      }
-      $json .= json_encode($obj['properties']['city_state']);
-      $i++;
-
-      $city_state_list[] = $obj['properties']['city_state'];
+$city_state_list = array();
+foreach ($cursor as $obj) {
+  if (!in_array($obj['properties']['city_state'], $city_state_list) && $obj['properties']['city_state'] != null) {
+    if($i > 0) {
+     $json .= ',';
     }
+    $json .= json_encode($obj['properties']['city_state']);
+    $i++;
+    
+    $city_state_list[] = $obj['properties']['city_state'];
   }
   
-  $json .= ']';
-  if($cursor_index < count($all_cursors) - 1) {
-   $json .= ', ';
-  }
   
-  $cursor_index++;
+  // $json .= json_encode(array(
+  //   'id' => $obj['id'],
+  //   'name' => $obj['properties']['name'],
+  // ));
 }
+/* $json .= "," . json_encode(array('count' => $collection->count())); */
 
-$json .= '}';
+$json .= ']';
 
 echo $json;
 
