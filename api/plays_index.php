9ec3af68697c4c920c69d00f6315c71569564024
Colin Sagan <colin@quilted.coop>
2012-07-11 11:02:48 -0700

    Make plays index use events data instead of plays collection. That way only plays that have events show up.

--- api/plays_index.php
+++ api/plays_index.php
@@ -2,7 +2,7 @@
 include('../../authentication/newplaymap_authentication.php');
 connectMongo(false);
 
-$collection = $m->newplaymap->plays;
+$collection = $m->newplaymap->events;
 
 // find everything in the collection
 $cursor = $collection->find()->sort(array("properties.play_title" => 1));
@@ -14,28 +14,33 @@ header("Last-Modified: " . gmdate( "D, d M Y H:i:s" ) . "GMT");
 header("Cache-Control: no-cache, must-revalidate"); 
 header("Pragma: no-cache");
 header("Content-type: application/json");
- $page;
+
 $json = '[ ' ;
 
 $i = 0;
 
 // iterate through the results
+$unique_plays = array();
 foreach ($cursor as $obj) {
   if(!empty($obj['id']) && !empty($obj['properties']['play_title'])) {
-    if($i > 0) {
-     $json .= ',';
+    if (!in_array($obj['properties']['play_title'], $unique_plays)) {
+      if($i > 0) {
+       $json .= ',';
+      }
+      $unique_plays[] = $obj['properties']['play_title'];
+
+      $json .= json_encode(array(
+        'value' => $obj['properties']['play_title_display'],
+        'label' => $obj['properties']['play_title_display'],
+        'path' => $obj['properties']['related_play_path'],
+      ));
+
+      $i++;
     }
 
-    // $json .= json_encode($obj['properties']['play_title']);
-    $json .= json_encode(array(
-      'value' => $obj['properties']['play_title'],
-      'label' => $obj['properties']['play_title_display'],
-      'path' => $obj['properties']['path'],
-    ));
-    
-    $i++;
   }
 }
+
 /* $json .= "," . json_encode(array('count' => $collection->count())); */
 
 $json .= ']';
