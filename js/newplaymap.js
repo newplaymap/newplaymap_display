89c0d15ae1148351565f4c66f3dc478510c59b37
Colin Sagan <colin@quilted.coop>
2012-09-29 23:55:05 -0700

    Refactor how the first tour slide is reset for IE wackiness.

--- js/newplaymap.js
+++ js/newplaymap.js
@@ -572,6 +572,7 @@ newPlayMap.tourInteraction = function() {
 newPlayMap.tourStart = function() {
   $('#tour').show();
   $('#tour-overlay').show();
+  $('.tour-step.first').addClass('active').show();
   // Add the arrows for the first slide
   newPlayMap.setTourArrows();
   // $.cookie('newplaymap_tour', true);
@@ -579,7 +580,6 @@ newPlayMap.tourStart = function() {
 
 newPlayMap.tourStop = function() {
   $('.tour-step').removeClass('active').hide();
-  $('.tour-step:first').addClass('active').show();
   $('#tour').hide();
   $('.tour-arrows').remove();
   $('#tour-overlay').hide();
